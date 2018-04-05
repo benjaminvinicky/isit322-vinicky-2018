67c9433d14d066ffd41c3103a5efd85dd7583aa9
Your Name <you@example.com>
2018-04-05 16:22:16 -0700

    Until Break progress in class on Thursday

--- Week01-React-Jest/src/App.js
+++ Week01-React-Jest/src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+          nine: '0'
+      }
+  }
+
+  getNine = () => {
+      console.log('state');
+      this.setState({nine: '9'})
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +22,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p className='elf'>State: {this.state.nine}</p>
+          <button className='elf' onClick={this.getNine}>Get Nine</button>
       </div>
     );
   }
--- Week01-React-Jest/src/App.test.js
+++ Week01-React-Jest/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+import ElfDebugEnzyme from '../ElfDebugEnzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const elfDebugEnzyme = new ElfDebugEnzyme(true, 'App.test.js');
+
+configure({ adapter: new Adapter() });
+
+describe('jest test', function() {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders and reads H1 text', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <h1 className="App-title">Welcome to React</h1>;
+        elfDebugEnzyme.getFirst(wrapper, 'h1', true);
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
 });
