4ac5d6c11917dfa7d25fab4dcd7b727748b5ff46
Your Name <you@example.com>
2018-04-08 20:28:02 -0700

    made changes with eslint. Now no errors!

--- Week01-React-Jest/src/App.js
+++ Week01-React-Jest/src/App.js
@@ -3,29 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-      super();
-      this.state = {
-          nine: '0'
-      }
-  }
+    constructor() {
+        super();
+        this.state = {
+            nine: '0'
+        };
+    }
 
   getNine = () => {
       console.log('state');
-      this.setState({nine: '9'})
+      this.setState({nine: '9'});
   };
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-          <p className='nine'>State: {this.state.nine}</p>
-          <button className='elf' onClick={this.getNine}>Get Nine</button>
-      </div>
-    );
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className='nine'>State: {this.state.nine}</p>
+              <button className='elf' onClick={this.getNine}>Get Nine</button>
+          </div>
+      );
   }
 }
 
