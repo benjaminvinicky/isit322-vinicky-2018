66afd59de3f51b2305b7b89d2ac489099e0fce8a
Your Name <you@example.com>
2018-04-23 20:39:47 -0700

    Finished the es6 assignment.

--- Week03-EcmaScript/work.js
+++ Week03-EcmaScript/work.js
@@ -16,8 +16,32 @@ let primeNumbers = (numbers) => {
 let primeArray = ([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]);
 primeNumbers(primeArray);
 
+const myObjects = { firstName: 'Benjamin', lastName: 'Vinicky', address: '1020 108 AVE NE APT#1305', city: 'Bellevue', state: 'WA', zip: '98004'}
 const address = () => {
-    for (let attribute in something) {
-
+    for (let property in myObject) {
+        if (myObject.hasOwnProperty(property)) {
+            console.log(property);
+        }
     }
 };
+
+class Calculator {
+    add(operanda, operandab) {
+        return this.operanda + this.operandb;
+    }
+    subtract(operanda, operandab) {
+        return this.operanda - this.operandb;
+    }
+    multiply(operanda, operandab) {
+        return this.operanda * this.operandb;
+    }
+    divide(operanda, operandab) {
+        return this.operanda / this.operandb;
+    }s
+}
+
+var calculator = new Calculator();
+console.log(calculator.add(2,3));
+console.log(calculator.subtract(5,2));
+console.log(calculator.multiply(3,5));
+console.log(calculator.divide(15,3));
